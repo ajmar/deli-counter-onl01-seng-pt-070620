519b83407b0883e01b05d1dfcf740f11ad86552f
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 04:02:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(deli)
   if deli.length == 0
     puts "The line is currently empty."
   elsif deli.length > 0
-    deli.each.with_index do |name, i|
+    deli.each.with_index(1) do |name, i|
       puts "The line is currently: #{i}. #{name}"
     end
   end
